aa778c4bb2dcaa382a2406350a06b3167ad65681
Roland Fuhrer <roland@resterland.ch>
2021-02-05 10:07:34 +0100

    update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,22 +5,17 @@
  */
 $EM_CONF[$_EXTKEY] = [
     'title' => 'ResterLand WebShop',
-    'description' => 'his extension extends the Aimeos webshop extension.',
-        'category' => 'plugin',
-        'constraints' => [
-            'depends' => [
-                'typo3' => '10.4.0-10.9.99',
-                'aimeos' => '20.4.0-20.99.99'
-            ],
-            'conflicts' => [],
-            'suggests' => [],
+    'description' => 'his extension extends the cat webshop extension.',
+    'category' => 'plugin',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '10.4.0-10.9.99'
         ],
-        'state' => 'beta',
-<<<<<<< HEAD
-    'version' => '1.1.0',
-=======
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+    'state' => 'beta',
     'version' => '2.1.0',
->>>>>>> develop
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
